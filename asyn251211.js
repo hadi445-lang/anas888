6c123d7d56c12cf906d3b454f5f6b9c482ed7c11
Github <github@aptech.com>
2025-12-11 12:52:59 +0500

    updated code

--- asyn251211.js
+++ asyn251211.js
@@ -1,6 +1,12 @@
-function Asynfunction(){
-    console.log("sir apki salad aur raita");
-    settimeout( () => {
-        
-    })
+function Asynfunction() {
+    console.log("Sir apki salad aur raita");
+
+    setTimeout(() => {
+        console.log("Sir apki Mutton Karahi agai hai");
+    }, 5000); // 5000 == 5 secs
+
+    console.log("Sir Pani ki bottle");
 }
+
+Asynfunction(); // immediate execution
+
